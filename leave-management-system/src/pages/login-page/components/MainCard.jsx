431b85f3c6119b952dc5e5b76d3e83204920a591
maneeshtej <varmaneesh@Sankars-MacBook-Air.local>
2024-07-05 12:19:01 +0530

    Created leave-management-system vite + react project Tailwind has been setup src/pages has been created src/pages/login-page has been created src/pages/login-page/assets has been created src/pages/login-oage/components has been created Created 3 files in src/pages/login-page/components/ LoginPage.jsx HeaderLogin.jsx MainCard.jsx Started rudimentary work on LoginPage - Maneesh

---
+++ leave-management-system/src/pages/login-page/components/MainCard.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function MainCard() {
+  return (
+    <div className="h-[500px] w-[500px] flex justify-center items-center">
+      Hello
+    </div>
+  );
+}
+
+export default MainCard;
